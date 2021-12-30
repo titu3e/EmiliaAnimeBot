4fefa6e73d83064adfe30d50e6c6544cdd5bae57
titu3e <75441242+titu3e@users.noreply.github.com>
2021-12-30 17:09:06 +0530

    Update config.py

--- EmiliaAnimeBot/config.py
+++ EmiliaAnimeBot/config.py
@@ -25,6 +25,7 @@ class Config(object):
     EVENT_LOGS = -1001598625668  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     LOG_GROUP_ID = -1001598625668
     ARQ_API_KEY = "AEWDWZ-AUZSMK-DWRYWB-VUBMGC-ARQ"
+    MONGO_DB_URI = "mongodb+srv://dbmab:eUtbAuei6mpLoinz@cluster0.r2c3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     
     #RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'postgres://mazxusdg:qON-42EN_Nnv-PQm5uqzt27ExGKPbCX0@john.db.elephantsql.com/mazxusdg'  # needed for any database modules
